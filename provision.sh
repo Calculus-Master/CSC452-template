f7621ae42872b3a0d34666a75df3dc0c722f44ff
Saptarshi Mallick <sapmallick@gmail.com>
2024-09-26 13:16:35 -0700

    Fixed provision file

--- provision.sh
+++ provision.sh
@@ -1,6 +1,6 @@
-apt-get update
-sudo apt install gcc
-sudo apt-get install build-essential
+sudo apt-get -y update
+sudo apt-get -y install gcc
+sudo apt-get -y install build-essential
 cd /vagrant/usloss
 ./configure
 make install
